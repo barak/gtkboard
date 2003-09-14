c6c95e7400ec9286ecaf12d093c478824aaa52b3
arvindn <arvindn>
2003-09-14 09:57:16 +0000

    *** empty log message ***

--- src/chess.c
+++ src/chess.c
@@ -23,6 +23,7 @@
 
 #include "game.h"
 #include "../pixmaps/chess.xpm"
+#include "move.h"
 
 #define CHESS_CELL_SIZE 54
 #define CHESS_NUM_PIECES 12
@@ -42,6 +43,24 @@
 #define CHESS_BB 10
 #define CHESS_BN 11
 #define CHESS_BP 12
+
+#define A_FILE 0
+#define B_FILE 1
+#define C_FILE 2
+#define D_FILE 3
+#define E_FILE 4
+#define F_FILE 5
+#define G_FILE 6
+#define H_FILE 7
+
+#define RANK_1 0
+#define RANK_2 1
+#define RANK_3 2
+#define RANK_4 3
+#define RANK_5 4
+#define RANK_6 5
+#define RANK_7 6
+#define RANK_8 7
 
 #define CHESS_ISWHITE(x) (x >= 1 && x <= 6)
 #define CHESS_ISBLACK(x) (x >= 7 && x <= 12)
@@ -132,27 +151,32 @@ void chess_init ()
 
 void *chess_newstate (Pos *pos, byte *move)
 {
-	static Chess_state state = {0, 0, 0, 0, -1};
+	static Chess_state state = {1, 1, 1, 1, -1};
 	if (!pos->state) return &state;
 	memcpy (&state, pos->state, sizeof (Chess_state));
 
-	// if pawn moves two squares to 4th rank then set epf
-	// FIXME: all this is useless until state can be sent
-	// from server to client
 	{
-	int val;
-	if (move[2] != 0) val = move[2];
-	else if (move[5] != 0) val = move[5];
+	int val, to_x, to_y;
+	// if pawn moves two squares to 4th rank then set epf
+	if (move[2] != 0) { val = move[2]; to_x = move[0]; to_y = move[1]; }
+	else if (move[5] != 0) { val = move[5]; to_x = move[3]; to_y = move[4]; }
 	else assert (0);
 	if (
 		(val == CHESS_WP 
-			&& ((move[0] == 1 && move[3] == 3) || (move[0] == 2 && move[3] == 1)))
+			&& ((move[1] == 1 && move[4] == 3) || (move[1] == 3 && move[4] == 1)))
 		||
-		(val == CHESS_WP 
-			&& ((move[0] == 1 && move[3] == 3) || (move[0] == 2 && move[3] == 1)))
+		(val == CHESS_BP 
+			&& ((move[1] == 6 && move[3] == 4) || (move[1] == 6 && move[4] == 6)))
 	)
-		state.epfile = move[1];
+		state.epfile = move[0];
 	else state.epfile = -1;
+
+	if (val == CHESS_WK) state.castle_WK = state.castle_WQ = 0;
+	if (val == CHESS_BK) state.castle_BK = state.castle_BQ = 0;
+	if (val == CHESS_WR && to_x >= 4) state.castle_WK = 0;
+	if (val == CHESS_WR && to_x <  4) state.castle_WQ = 0;
+	if (val == CHESS_BR && to_x >= 4) state.castle_BK = 0;
+	if (val == CHESS_BR && to_x <  4) state.castle_BQ = 0;
 	} 
 	
 	return &state;
@@ -171,7 +195,7 @@ static int isfreeline (byte *pos, int oldx, int oldy, int newx, int newy)
 	return 1;	
 }
 
-static int islegal (Pos *pos, int oldx, int oldy, int x, int y, int player)
+static int islegal (Pos *pos, int oldx, int oldy, int x, int y)
 {
 	int piece = pos->board [oldy * board_wid + oldx];
 	byte *board = pos->board;
@@ -237,26 +261,78 @@ static int islegal (Pos *pos, int oldx, int oldy, int x, int y, int player)
 			}
 		case CHESS_WP:
 			if (board [y * board_wid + x] == 0)
+			{
+				// en passant
+				if (y == 5 && oldy == 4 && (x == oldx + 1 || x == oldx - 1)
+						&& board [oldy * board_wid + x] == CHESS_BP)
+					return 1;
 				return (x == oldx && (y == oldy + 1 || 
 						(y == 3 && oldy == 1 && board [(2*board_wid + x)] == 0)));
+			}
 			return y == oldy + 1  && (x == oldx + 1 || x == oldx - 1) ;
 		case CHESS_BP:
 			if (board [y * board_wid + x] == 0)
+			{
+				if (y == 4 && oldy == 5 && (x == oldx + 1 || x == oldx - 1)
+						&& board [oldy * board_wid + x] == CHESS_WP)
+					return 1;
 				return (x == oldx && (y == oldy - 1 ||
 						(y == 4 && oldy == 6 && board [5*board_wid + x] == 0)));
+			}
 			return y == oldy - 1  && (x == oldx + 1 || x == oldx - 1) ;
 		default:
 			return 1;
 	}
 }
 
-static int oppcolor (byte *pos, int oldx, int oldy, int x, int y)
+static int oppcolor (byte *board, int oldx, int oldy, int x, int y)
 	/* True if one square is W and the other is B */
 {
-	int oldv = pos [oldy * board_wid + oldx], v = pos [y * board_wid + x];
+	int oldv = board [oldy * board_wid + oldx], v = board [y * board_wid + x];
 	if (CHESS_ISWHITE (oldv) && CHESS_ISBLACK (v)) return 1;
 	if (CHESS_ISBLACK (oldv) && CHESS_ISWHITE (v)) return 1;
 	return 0;
+}
+
+static gboolean is_in_check (Pos *pos, Player player)
+{
+	int i, j, kingx = -1, kingy = -1;
+	for (i=0; i<board_wid; i++)
+	for (j=0; j<board_heit; j++)
+	{
+		int val = pos->board [j * board_wid + i];
+		if ((player == WHITE && val == CHESS_WK) || 
+				(player == BLACK && val == CHESS_BK))
+		{
+			kingx = i;
+			kingy = j;
+		}
+	}
+	
+	for (i=0; i<board_wid; i++)
+	for (j=0; j<board_heit; j++)
+	{
+		int val = pos->board [j * board_wid + i];
+		if (!((player == WHITE && CHESS_ISBLACK (val)) || 
+				(player == BLACK && CHESS_ISWHITE (val))))
+			continue;
+		if (islegal (pos, i, j, kingx, kingy))
+			return TRUE;
+	}
+	return FALSE;
+}
+
+//! Would "player" be in check after making "move"
+static gboolean leads_to_check (byte *board, byte *move, Player player)
+{
+	static byte newboard [CHESS_BOARD_WID*CHESS_BOARD_HEIT];
+	Pos pos;
+	memcpy (newboard, board, board_wid * board_heit);
+	move_apply (newboard, move);
+	pos.board = newboard;
+	pos.state = NULL;
+	return is_in_check (&pos, player);
+
 }
 
 static int oldx = -1, oldy = -1, oldval = -1;
@@ -305,6 +381,8 @@ int chess_getmove (Pos *pos, int x, int y, GtkboardEventType type, Player player
 		move[4] = (player == WHITE ? 7 : 0);
 		move[5] = new_p;
 		move[6] = -1;
+
+		if (leads_to_check (pos->board, move, player)) return -1;
 		
 		if (movep)
 			*movep = move;
@@ -332,10 +410,24 @@ int chess_getmove (Pos *pos, int x, int y, GtkboardEventType type, Player player
 		return -1;
 	}
 
-	if (!islegal (pos, oldx, oldy, x, y, player))
+	if (!islegal (pos, oldx, oldy, x, y))
 	{
 		oldx = -1; oldy = -1;
 		return -1;
+	}
+
+	/* en passant */
+	if ((oldval == CHESS_WP || oldval == CHESS_BP) && val == 0
+			&& x != oldx)
+	{
+		move[0] = oldx; move[1] = oldy; move[2] = 0;
+		move[3] = x; move[4] = y; move[5] = oldval;
+		move[6] = x; move[7] = oldy; move[8] = 0;
+		move[9] = -1;
+		if (leads_to_check (pos->board, move, player)) return -1;
+		if (movep)
+			*movep = move;
+		return 1;
 	}
 
 	/* pawn promotion */
@@ -361,6 +453,7 @@ int chess_getmove (Pos *pos, int x, int y, GtkboardEventType type, Player player
 		move[9] = rfile; move[10] = y; move[11] = 0;
 		move[12] = -1;
 		oldx = -1, oldy = -1;
+		if (leads_to_check (pos->board, move, player)) return -1;
 		if (movep)
 			*movep = move;
 		return 1;
@@ -371,12 +464,11 @@ int chess_getmove (Pos *pos, int x, int y, GtkboardEventType type, Player player
 	move[6] = -1;
 
 	oldx = -1; oldy = -1;
+	if (leads_to_check (pos->board, move, player)) return -1;
 	
 	if (movep)
 		*movep = move;
 	return 1;
-	/*if (player == WHITE && CHESS_ISWHITE (val))
-		return 0;*/
 }
 
 static int hasmove (Pos *pos, int player)
@@ -390,7 +482,7 @@ static int hasmove (Pos *pos, int player)
 		if (player == BLACK && !CHESS_ISBLACK (val)) continue;
 		for (x2 = 0; x2 < board_wid; x2++)
 		for (y2 = 0; y2 < board_heit; y2++)
-			if (islegal (pos, x1, y1, x2, y2, player)) return 1;
+			if (islegal (pos, x1, y1, x2, y2)) return 1;
 	}
 	return 0;
 }
@@ -398,9 +490,11 @@ static int hasmove (Pos *pos, int player)
 ResultType chess_who_won (Pos *pos, Player player, char **commp)
 {
 	static char comment[32];
-	char *who_str [3] = { "white won", "black won", "draw" };
+	char *who_str [3] = { "White won", "Black won", "Draw" };
+	byte *move_list;
 	*commp = NULL;
-	if (hasmove (pos, player)) //return RESULT_NOTYET;
+	move_list = chess_movegen (pos, player);
+	if (move_list[0] != -2)
 	{
 		if (pos->num_moves > chess_max_moves)
 		{
@@ -412,10 +506,10 @@ ResultType chess_who_won (Pos *pos, Player player, char **commp)
 		return RESULT_NOTYET;
 	}
 	*commp = comment;
-	if (hasmove (pos, player == WHITE ? BLACK : WHITE))
+	if (is_in_check (pos, player))
 	{
-		strncpy (comment, who_str[player == WHITE ? 0 : 1], 31);
-		return player == WHITE ? RESULT_WHITE : RESULT_BLACK;
+		strncpy (comment, who_str[player == WHITE ? 1 : 0], 31);
+		return player == WHITE ? RESULT_BLACK : RESULT_WHITE;
 	}
 	else 
 	{
@@ -424,12 +518,12 @@ ResultType chess_who_won (Pos *pos, Player player, char **commp)
 	}
 }
 
-int chess_movegen_square (byte *pos, byte **movp, int player,
+int chess_movegen_square (byte *board, byte **movp, int player,
 		int oldx, int oldy, int x, int y)
 {
 	int val;
 	if (!ISINBOARD(x, y)) return 0;
-	val = pos [y * board_heit + x];
+	val = board [y * board_heit + x];
 	if ((player == WHITE && CHESS_ISWHITE (val))
 		|| (player == BLACK && CHESS_ISBLACK (val)))
 		return 0;
@@ -438,8 +532,13 @@ int chess_movegen_square (byte *pos, byte **movp, int player,
 	*(*movp)++ = 0;
 	*(*movp)++ = x;
 	*(*movp)++ = y;
-	*(*movp)++ = pos [oldy * board_heit + oldx];
+	*(*movp)++ = board [oldy * board_heit + oldx];
 	*(*movp)++ = -1;
+	if (leads_to_check (board, *movp-7, player))
+	{
+		*movp -= 7;
+		return 0;
+	}
 	return 1;
 }
 
@@ -477,6 +576,145 @@ static void chess_movegen_promote (byte *board, byte **movp, int player,
 		*(*movp)++ = y;
 		*(*movp)++ = promote_pieces[i][j];
 		*(*movp)++ = -1;
+		if (leads_to_check (board, *movp-7, player))
+		{
+			*movp -= 7;
+			return;
+		}
+	}
+}
+
+static void chess_movegen_enpassant (Pos *pos, byte **movp, int player,
+		int x, int y)
+{
+	int val = pos->board [y * board_wid + x];
+	int epfile;
+	if (!pos->state) epfile = -1;
+	else epfile = ((Chess_state *)pos->state)->epfile;
+	if (epfile < 0)
+		return;
+	if ((player == WHITE && val != CHESS_WP) || 
+			(player == BLACK && val != CHESS_BP))
+		return;
+	if ((player == WHITE && y != 4) ||  
+			(player == BLACK && y != 3))
+		return;
+	if (abs (x - epfile) != 1)
+		return;
+	*(*movp)++ = x;
+	*(*movp)++ = y;
+	*(*movp)++ = 0;
+	*(*movp)++ = epfile;
+	*(*movp)++ = y + (player == WHITE ? 1 : -1);
+	*(*movp)++ = val;
+	*(*movp)++ = epfile;
+	*(*movp)++ = y;
+	*(*movp)++ = 0;
+	*(*movp)++ = -1;
+	if (leads_to_check (pos->board, *movp-10, player))
+		*movp -= 10;
+}
+
+static void chess_movegen_castle (Pos *pos, byte **movp, int player)
+{
+	int rank = player == WHITE ? RANK_1 : RANK_8;
+	int king = player == WHITE ? CHESS_WK : CHESS_BK;
+	int rook = player == WHITE ? CHESS_WR : CHESS_BR;
+	int castle_k, castle_q;
+	byte *board = pos->board;
+ 	if (!pos->state) return;
+	castle_k = player == WHITE ? 
+		((Chess_state *)pos->state)->castle_WK : ((Chess_state *)pos->state)->castle_BK;
+	castle_q = player == WHITE ? 
+		((Chess_state *)pos->state)->castle_WQ : ((Chess_state *)pos->state)->castle_BQ;
+	if (board [rank * board_wid + E_FILE] != king)
+		return;
+	if (is_in_check (pos, player)) return;
+
+	if (board [rank * board_wid + H_FILE] == rook && castle_k)
+	{
+		gboolean allowed = TRUE;
+		*(*movp)++ = E_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = 0;
+		*(*movp)++ = F_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = king;
+		*(*movp)++ = -1;
+		if (leads_to_check (pos->board, *movp-7, player))
+			allowed = FALSE;
+		*movp -= 7;
+		
+		*(*movp)++ = E_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = 0;
+		*(*movp)++ = G_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = king;
+		*(*movp)++ = -1;
+		if (leads_to_check (pos->board, *movp-7, player))
+			allowed = FALSE;
+		*movp -= 7;
+
+		if (allowed)
+		{
+			*(*movp)++ = E_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = 0;
+			*(*movp)++ = G_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = king;
+			*(*movp)++ = H_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = 0;
+			*(*movp)++ = F_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = rook;
+			*(*movp)++ = -1;
+		}
+	}
+	
+	if (board [rank * board_wid + A_FILE] == rook && castle_q)
+	{
+		gboolean allowed = TRUE;
+		*(*movp)++ = E_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = 0;
+		*(*movp)++ = D_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = king;
+		*(*movp)++ = -1;
+		if (leads_to_check (pos->board, *movp-7, player))
+			allowed = FALSE;
+		*movp -= 7;
+		
+		*(*movp)++ = E_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = 0;
+		*(*movp)++ = C_FILE;
+		*(*movp)++ = rank;
+		*(*movp)++ = king;
+		*(*movp)++ = -1;
+		if (leads_to_check (pos->board, *movp-7, player))
+			allowed = FALSE;
+		*movp -= 7;
+
+		if (allowed)
+		{
+			*(*movp)++ = E_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = 0;
+			*(*movp)++ = C_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = king;
+			*(*movp)++ = A_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = 0;
+			*(*movp)++ = D_FILE;
+			*(*movp)++ = rank;
+			*(*movp)++ = rook;
+			*(*movp)++ = -1;
+		}
 	}
 }
 
@@ -504,6 +742,7 @@ byte *chess_movegen (Pos *pos, Player player)
 			switch (val)
 			{
 				case CHESS_WP:
+					chess_movegen_enpassant (pos, &movp, player, i, j);
 					if (ISINBOARD (i, j+1) && board [(j+1) * board_heit + i] == 0)
 					{
 						if (j == board_heit - 2)
@@ -539,6 +778,7 @@ byte *chess_movegen (Pos *pos, Player player)
 								i, j, i, j+2);
 					break;
 				case CHESS_BP:
+					chess_movegen_enpassant (pos, &movp, player, i, j);
 					if (ISINBOARD (i, j-1) && board [(j-1) * board_heit + i] == 0)
 					{
 						if (j == 1)
@@ -575,6 +815,7 @@ byte *chess_movegen (Pos *pos, Player player)
 					break;
 				case CHESS_WK:
 				case CHESS_BK:
+					chess_movegen_castle (pos, &movp, player);
 					for (k=0; k<4; k++)
 						chess_movegen_square(board, &movp, player, 
 								i, j, i + incxr[k], j + incyr[k]);
